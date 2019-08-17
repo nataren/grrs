27bdabe5dad0f23967577f3140db176beeeb05f6
César López-Natarén <cesar@nataren.io>
2019-08-16 23:43:03 -0700

    Parse the arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use structopt::StructOpt;
+use std::fmt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short = "n", long = "pattern")]
+    pattern: String,
+
+    #[structopt(short = "p", long = "path", parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+impl fmt::Display for Cli {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(pattern={}, path={})", self.pattern, self.path.display())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{}", args)
 }
